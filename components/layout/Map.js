f2a07e3791c5368e522d6e19fc27ccf66f5f14ee
Maciej <maciej@maziuk.com.pl>
2023-03-11 16:35:00 +0100

    /layout add google map

---
+++ components/layout/Map.js
@@ -0,0 +1,17 @@
+import classes from "./Map.module.css";
+const MapPage = () => {
+  return (
+    <iframe
+      className={classes.map}
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9383.307327092372!2d20.3943859!3d53.988098!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e2893fa549690b%3A0x1a40263e00e9d8fc!2sCUKIERNIA%20MAZIUK!5e0!3m2!1spl!2spl!4v1678232243025!5m2!1spl!2spl"
+      width="600"
+      height="400"
+      //   style="border:0;"
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+};
+
+export default MapPage;
