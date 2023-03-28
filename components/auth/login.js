9cf550125bc88560c9d07be626a0a890c41926b6
Maciej <maciej@maziuk.com.pl>
2023-03-28 02:35:01 +0200

    /components/auth add frontend auth component

---
+++ components/auth/login.js
@@ -0,0 +1,25 @@
+// PROPER FILE FOR GOOGLE AUTH
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const Login = () => {
+  const { data: session } = useSession();
+  console.log(session);
+  if (session) {
+    return (
+      <div>
+        <p>Witaj {session.user.name}!</p>
+        <button onClick={() => signOut()}>signOut</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>You are not signed in!</p>
+        <button onClick={() => signIn()}>SignIn</button>
+      </div>
+    );
+  }
+};
+
+export default Login;
